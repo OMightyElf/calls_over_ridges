86a7b5d0f208f00d6361f0308e4209f87c4862f7
Adele Yu <fhead17@gmail.com>
2015-11-29 19:15:45 +0800

    index page main issue is here

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
 	  @posts = Post.all
 	  @latest_posts = Post.all.order(created_at: :desc)
 	  @covers = @posts.first(4)
+	  @hot_posts = Post.all.order(view_count: :desc)
+	  @main_issue_posts = Post.all.order(created_at: :desc)
+	  # TODO main_issue 的 posts ~
 		# render layout: "layout_with_cover"
 	end
 
