b8fb23e6b2e5c3d7cd3578d704aff0629e464b79
Adele Yu <fhead17@gmail.com>
2017-01-12 01:55:52 +0800

    add publish scope

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(publish_date: :desc)
+    @posts = Post.all.publish.order(publish_date: :desc)
     @pinned_post_with_img = Post.first
     @pinned_post_with_video = Post.first
   end
 
   def show
     @post = Post.find(params[:id])
-    @latest_posts = Post.all.order(publish_date: :desc).limit(3)
+    @latest_posts = Post.all.publish.order(publish_date: :desc).limit(3)
   end
 
   def new
@@ -17,12 +17,5 @@ class PostsController < ApplicationController
   def create
     @post = Post.create(post_params)
     redirect_to @post
-  end
-
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :content, :summary, :cover, tag_id: [])
   end
 end
