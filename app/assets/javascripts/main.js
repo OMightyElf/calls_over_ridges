a40555705003dce6cea031efccf5abe23373ff73
Adele Yu <fhead17@gmail.com>
2017-01-12 01:37:01 +0800

    renew js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,6 +4,8 @@ $(function () {
 		$('body').toggleClass('menu--on');
 	});
 
+	$('.parallax').parallax();
+
 	$('[data-fancybox="true"]').fancybox();
 
 	$('[data-fancybox-close="true"]').click(function (e) {
@@ -15,4 +17,36 @@ $(function () {
 		e.preventDefault();
 		$(this).closest('form')[0].reset();
 	});
+	$(".dropdown-button").dropdown({belowOrigin:true });
+
+	//¼Æ¦reffect
+	function counter() {
+		$(window).scroll(function () {
+			$('.counter').each(function () {
+				if ($(window).scrollTop() > $(this).offset().top-$(window).height()) {
+					var $this = $(this).find('.counter__number'),
+						countTo = $this.attr('data-count'),
+						duration = Number($this.attr('data-duration')),
+						easing = $this.attr('data-easing');
+					if (!$this.data('star')) {
+						$({ countNum: $this.text() }).animate({
+							countNum: countTo
+						},
+						{
+							duration: duration,
+							easing: easing,
+							step: function () {
+								$this.text(Math.floor(this.countNum));
+							},
+							complete: function () {
+								$this.text(this.countNum);
+							}
+						});
+					};
+					$this.data('star', true);
+				}
+			});
+		}).trigger('scroll');
+	}
+	counter();
 });
