c8ed96817d1f8c5b9fe042e2153cd324103a7d8c
Adele Yu <fhead17@gmail.com>
2017-01-14 21:33:29 +0800

    change devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :updates
   resources :children
   ActiveAdmin.routes(self)
-  # devise_for :users
-  devise_for :users, controllers: { sessions: 'user/sessions' }
+  devise_for :users
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
