e76ac8d00c2d22fc9970d1144258549367a5d00f
Adele Yu <fhead17@gmail.com>
2017-01-14 23:06:43 +0800

    fix children and update admin

--- app/admin/child.rb
+++ app/admin/child.rb
@@ -8,7 +8,10 @@ ActiveAdmin.register Child do
 # or
 #
 	permit_params do
-		permitted = [:name, :serial_number, :gender, :birthday, :school, :school_year, :supported_at, :support_until, :supporter_id]
+		permitted = [:name, :serial_number, :gender, :birthday, :supported_at, :support_until, :supporter_id,
+									updates_attributes: [:update_time, :current_school, :current_grade, :attendence_rate,
+																				:reading_report_amount, :grade, :family_income, :weight,
+																				:height, :study_hours, :comment]]
 		permitted
 	end
 
@@ -22,15 +25,13 @@ ActiveAdmin.register Child do
 
 	show do
 		columns do
-			column span: 4 do
+			column span: 7 do
 				h1 "孩童檢視"
 				attributes_table  do
 					row :name
 					row :serial_number
 					row :gender
 					row :birthday
-					row :school
-					row :school_year
 					row :supported_at
 					row :support_until
 					row :user do |child|
@@ -38,6 +39,49 @@ ActiveAdmin.register Child do
 							column do |user|
 								link_to user.name, admin_user_path(user)
 							end
+						end
+					end
+				end
+			end
+			column span: 5 do
+				h1 "更新資料"
+				child.updates.each do |update|
+					attributes_table do
+						row :update_time do
+							update.update_time
+						end
+						row :current_school do
+							update.current_school
+						end
+						row :current_grade do
+							update.current_grade
+						end
+						row :attendence_rate do
+							update.attendence_rate
+						end
+						row :reading_report_amount do
+							update.reading_report_amount
+						end
+						row :grade do
+							update.grade
+						end
+						row :family_income do
+							update.family_income
+						end
+						row :weight do
+							update.weight
+						end
+						row :height do
+							update.height
+						end
+						row :study_hours do
+							update.study_hours
+						end
+						row :created_at do
+							update.created_at
+						end
+						row :updated_at do
+							update.updated_at
 						end
 					end
 				end
@@ -50,8 +94,6 @@ ActiveAdmin.register Child do
 		column :serial_number
 		column :gender
 		column :birthday
-		column :school
-		column :school_year
 		column :supported_at
 		column :support_until
 		column :supporter_id
@@ -63,25 +105,25 @@ ActiveAdmin.register Child do
 			f.semantic_errors *f.object.errors.keys
 			f.input :name
 			f.input :serial_number
-			f.input :gender, as: :select, collection: Child.genders.keys
-			f.input :birthday, as: :datetime_picker
-			f.input :school
-			f.input :school_year
+			f.input :gender, as: :select, collection: Child.genders.keys, include_blank: false;
+			f.input :birthday, as: :date_picker
 			f.input :supported_at, as: :date_picker
 			f.input :support_until, as: :date_picker
 			f.input :user, as: :select2, collection: User.all, member_label: Proc.new { |u| u.name }
 		end
 
 		f.has_many :updates, header: "月更新資料" do |update|
-			update.input :month
-			update.input :year
+			update.input :update_time, as: :datetime_picker, value: Time.now
+			update.input :current_school
+			update.input :current_grade
 			update.input :attendence_rate
 			update.input :reading_report_amount
 			update.input :grade
 			update.input :family_income
 			update.input :weight
 			update.input :height
-			update.input :reading_time
+			update.input :study_hours
+			update.input :comment
 		end
 		f.actions
 	end
--- app/admin/update.rb
+++ app/admin/update.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Update do
-	permit_params :month, :year, :attendence_rate, :reading_report_amount, :grade, :family_income, :weight, :height, :reading_time, :child_id
+	permit_params :update_time, :current_school, :current_grade, :attendence_rate, :reading_report_amount, :grade, :family_income, :weight, :height, :study_hours, :child_id
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -26,15 +26,16 @@ ActiveAdmin.register Update do
 			column span: 4 do
 				h1 "月更新資料"
 				attributes_table  do
-					row :month
-					row :year
+					row :update_time
+					row :current_school
+					row :current_grade
 					row :attendence_rate
 					row :reading_report_amount
 					row :grade
 					row :family_income
 					row :weight
 					row :height
-					row :reading_time
+					row :study_hours
 					row :created_at
 					row :updated_at
 					row :child do |update|
@@ -50,15 +51,16 @@ ActiveAdmin.register Update do
 	end
 
 	index do
-		column :month
-		column :year
+		column :update_time
+		column :current_school
+		column :current_grade
 		column :attendence_rate
 		column :reading_report_amount
 		column :grade
 		column :family_income
 		column :weight
 		column :height
-		column :reading_time
+		column :study_hours
 		column :created_at
 		column :updated_at
 		column :child do |update|
@@ -77,15 +79,16 @@ ActiveAdmin.register Update do
 			f.semantic_errors *f.object.errors.keys
 
 			f.input :child, as: :select2, collection: Child.all, include_blank: false
-			f.input :month
-			f.input :year
+			f.input :update_time
+			f.input :current_school
+			f.input :current_grade
 			f.input :attendence_rate
 			f.input :reading_report_amount
 			f.input :grade
 			f.input :family_income
 			f.input :weight
 			f.input :height
-			f.input :reading_time
+			f.input :study_hours
 		end
 		f.actions
 	end
