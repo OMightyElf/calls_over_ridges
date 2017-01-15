f9cce93fe7a720a1a977ef55cd0cc3de87f68fa3
Adele Yu <fhead17@gmail.com>
2017-01-15 23:23:06 +0800

    forgot zipcode in permit params, add required to current password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,8 +8,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:name, :password, :password_confirmation, :address, :phone_number, :need_receipt]
-    devise_parameter_sanitizer.for(:account_update) << [:name, :password, :password_confirmation, :address, :phone_number, :need_receipt]
+    devise_parameter_sanitizer.for(:sign_up) << [:name, :password, :password_confirmation, :address, :phone_number, :need_receipt, :zipcode]
+    devise_parameter_sanitizer.for(:account_update) << [:name, :password, :password_confirmation, :address, :phone_number, :need_receipt, :zipcode]
   end
 
   def after_update_path_for(resource)
