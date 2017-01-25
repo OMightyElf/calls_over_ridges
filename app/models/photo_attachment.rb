c169aafd8ee9ac65f7ea689e32734ed24697dd97
Adele Yu <fhead17@gmail.com>
2017-01-25 13:59:18 +0800

    how about this......

--- app/models/photo_attachment.rb
+++ app/models/photo_attachment.rb
@@ -1,6 +1,6 @@
 class PhotoAttachment < ActiveRecord::Base
    # has_many :post_attachments
    # accepts_nested_attributes_for :post_attachments
-    belongs_to :update
+    belongs_to :monthly_update, foreign_key: 'update_id', class_name: 'Update'
 	  mount_uploader :picture, PictureUploader
 end
