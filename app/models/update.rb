30907ace60eb314b4f33059b4fb8e878a59a21bb
Adele Yu <fhead17@gmail.com>
2017-01-25 12:53:30 +0800

    how about this

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,7 +1,7 @@
 class Update < ActiveRecord::Base
 	belongs_to :child
 	has_many :photos, foreign_key: 'update_id', class_name: 'PhotoAttachment'
-	accepts_nested_attributes_for :photo_attachments
+	accepts_nested_attributes_for :photos
 
 	validates :child_id, presence: true
 end
