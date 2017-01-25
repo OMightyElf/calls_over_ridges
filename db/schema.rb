91dce4d43ee486b3ec4f5efe5412e20ee8b5636d
Johnny Wang <johnny.kwwang@gmail.com>
2017-01-26 00:10:10 +0800

    目前很陽春  就是選很多張{serial_number}_{xxx}.jpg的照片上傳 就會塞進選的日期的update裡面 成功的話會把上傳成功的照片顯示在畫面上

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124182925) do
+ActiveRecord::Schema.define(version: 20170125150907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20170124182925) do
     t.string   "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "update_id"
   end
 
   create_table "posts", force: :cascade do |t|
