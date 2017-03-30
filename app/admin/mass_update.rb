1ef0f47dab33bc1625f9a7e894dbe64111ab72d4
Adele Yu <fhead17@gmail.com>
2017-03-30 22:47:02 +0800

    remove /100

--- app/admin/mass_update.rb
+++ app/admin/mass_update.rb
@@ -22,7 +22,7 @@ ActiveAdmin.register_page "Mass Update" do
         u.update_month = row[1]
         u.current_school = row[3]
         u.current_grade = row[4]
-        u.attendence_rate = row[5].to_f/100  #attendance rate
+        u.attendence_rate = row[5].to_f  #attendance rate
         u.reading_report_amount = row[6].to_i #reading reports
         u.grade = row[7].to_i #grade
         u.family_income = row[8].to_i #family incone
