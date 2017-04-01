397ba65462d90159ebacc6384c7b722e04f32d22
Adele Yu <fhead17@gmail.com>
2017-04-01 21:15:29 +0800

    add redirection if go to wrong page

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -4,10 +4,13 @@ class ChildrenController < ApplicationController
   def show
     @user = current_user
     @child = Child.find(params[:id])
-    authorize @child
-    updates = @child.updates.order(update_year: :desc, update_month: :desc).limit(6)
-    @update = updates.first
-    @last_update = updates.first(2).last
-    @message = Message.new
+    if authorize @child
+      updates = @child.updates.order(update_year: :desc, update_month: :desc).limit(6)
+      @update = updates.first
+      @last_update = updates.first(2).last
+      @message = Message.new
+    else
+      redirect_to @user.children.first
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    authorize @user
-    @child = @user.first_child
-    @message = Message.new
+    if authorize @user
+      @child = @user.first_child
+      @message = Message.new
+    else
+      redirect_to current_user
+    end
   end
 
   def index
